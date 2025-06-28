688fc755fb2fa837e33c0717b91db2f58f185a22
Palak952 <pg1577652@gmail.com>
2025-06-28 11:55:40 +0530

    Fix: Updated Transaction entity with validation and column mappings

--- src/main/java/com/example/usercrud/model/Transaction.java
+++ src/main/java/com/example/usercrud/model/Transaction.java
@@ -1,6 +1,9 @@
 package com.example.usercrud.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,13 +18,28 @@ import java.time.LocalDateTime;
 public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "transaction_Id")
     private Long id;
 
+    @NotNull(message = "Please Enter Amount ")
+    @Positive(message = "Amount must be positive")
+    @Column(name = "amount", nullable = false)
     private Double amount;
+
+    @NotBlank(message = "Please Enter Category ")
+    @Column(name = "category", nullable = false)
     private String category;
+
+    @NotBlank(message = "Please Enter type (Income/Expense/Transfer)")
+    @Column(name = "type", nullable = false)
     private String Type;
+
+    @Column(name = "note")
     private String note;
+
+    @Column(updatable = false)
     private LocalDateTime createAt;
+
     private LocalDateTime updateAt;
 
     @PrePersist
